b3fb8532872de1996cfaf9ddd303ce3726d119ca
programmer-nur <pesfootball83@gmail.com>
2025-10-06 11:17:04 +0600

    added if elase conditon

---
+++ Module-2/if_else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int tk;
+
+    scanf("%d" , &tk);
+
+    if(tk >= 100){
+        printf("Burger Khabo");
+    }else{
+        printf("Burger Khabo na");
+    }
+}
---
+++ Module-2/relational.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int main (){
+    int a = 6, b = 10;
+
+    
+}
