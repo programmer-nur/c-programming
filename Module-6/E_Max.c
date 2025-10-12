df3bf92007340871a85cc86006fa97e1a6bacb97
programmer-nur <pesfootball83@gmail.com>
2025-10-12 23:20:32 +0600

    add max

---
+++ Module-6/E_Max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x;
+    int max = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &x);
+        if (x > max)
+        {
+            max = x;
+        }
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
